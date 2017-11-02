0c7024c12f505add8c111e0c46588919a32f70c0
[Hello World] <chaungo@users.noreply.github.com>
2017-11-02 10:25:43 +0700

    update structure
    update structure

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 var app = angular.module('App', ['ngMaterial', 'ngResource', 'ngAnimate']);
-app.controller('AppCtrl', function ($scope,$mdSidenav) {
-    $scope.toggleSidenav = function() {
+app.config(function ($mdThemingProvider) {
+    $mdThemingProvider.theme('default').primaryPalette('blue').accentPalette('red');
+
+});
+
+app.controller('AppCtrl', function ($scope, $mdSidenav) {
+    $scope.toggleSidenav = function () {
         return $mdSidenav('left').toggle();
     };
 });
